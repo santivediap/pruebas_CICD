277c549723e8089ccbde0c0423f4531d2d35f31f
SVega9848 <santivediap@gmail.com>
2023-06-30 11:03:41 +0200

    Erro aposta

--- src/math.js
+++ src/math.js
@@ -1,8 +1,8 @@
 const sum = (a, b) => a + b;
 const mul = (a, b) => a * b;
-const sub = (a, b) => a - b;
+const sub = (a, b) => a - b
 const div = (a, b) => a / b;
-const calculatePercentage = (num, percentage) => (num*percentage)/100;
+const calculatePercentage = (num, percentage) => (num*percentage)/100
 
-module.exports = { sum, mul, sub, div, calculatePercentage };
-console.log(("hola"));
+module.exports = { sum, mul, sub, div, calculatePercentage }
+console.log(("hola"))
